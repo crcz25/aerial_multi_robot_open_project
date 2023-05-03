01142f3c6cd0e53e629ee05a1b22a715782b0e7f
Henry Ascencio <ascencio.henan@gmail.com>
2023-05-03 12:10:55 +0300

    add the worlds folder to the share install folder when building the robots_description package

--- src/robots_description/setup.py
+++ src/robots_description/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'robots_description'
 
@@ -7,9 +9,15 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name]
+        ),
         ('share/' + package_name, ['package.xml']),
+        (
+            os.path.join('share', package_name, 'worlds'),
+            glob('worlds/*.world')
+        )
     ],
     install_requires=['setuptools'],
     zip_safe=True,
